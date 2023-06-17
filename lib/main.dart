ae8b5125264d5e66ef70e5a228bed0515a56dc7e
ElisMcc <elimcc@gmail.com>
2023-06-17 18:00:03 -0500

    Registro y listado alumnos

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:justificaciones/src/bloc/provider.dart';
+import 'package:justificaciones/src/pages/alumno_page.dart';
 import 'package:justificaciones/src/pages/home.dart';
 
 import 'package:justificaciones/src/pages/home_page.dart';
 import 'package:justificaciones/src/pages/login_page.dart';
-import 'package:justificaciones/src/pages/alumno_page.dart';
 import 'package:justificaciones/src/pages/registro_page.dart';
 import 'package:justificaciones/src/preferencias_usuario/preferencias_usuario.dart';
 
